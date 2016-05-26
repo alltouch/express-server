8ba2eb20a785ef0e678c6634394901a83a86bd65
Kirill Shavaran <alltouch@ukr.net>
2016-05-26 20:56:52 +0300

    dynamic pages

--- routes.js
+++ routes.js
@@ -1,13 +1,57 @@
-
-var lines = [];
+var articles = [
+    {
+        id: 'new1',
+        title: 'This is first post'
+    },
+    {
+        id: 'new2',
+        title: 'This is second post'
+    },
+    {
+        id: 'new3',
+        title: 'This is third post'
+    },
+    {
+        id: 'new4',
+        title: 'This is fourth post'
+    },
+    {
+        id: 'new5',
+        title: 'This is fifth post'
+    }
+];
 
 module.exports = function (app) {
 
     app.get('/', function (req, res) {
         res.render('index.hbs', {
-            title: 'Type your name man'
+            title: 'This is homepage',
+            articles: articles
         });
     });
+
+    app.get('/new:id', function (req, res) {
+        var id = 'new' + req.params.id;
+        var xxx = articles.filter(function (item) {
+            return item.id === id;
+        });
+        if (!xxx.length) {
+            return res.status(404).end('404 not found');
+        }
+        res.render('item.hbs', {
+            date: new Date().toString(),
+            title: xxx[0].title,
+            body: 'This is big body'
+        });
+    });
+
+    /*app.get('/new2', function (req, res) {
+     res.render('item.hbs', {
+     date: new Date().toString(),
+     title: 'This is second post',
+     body: 'lorem ipsum fjsdhgdsghsdjgdhgkjdsgksdhgdskjg'
+     });
+     });*/
 
     app.post('/form-submit', function (req, res) {
         var name = req.body.name;
